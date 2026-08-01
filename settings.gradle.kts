1b0ee3a42a3ab4edd374ef2271bb92cf69807957
MarioS271 <marioerik.scholze@gmail.com>
2026-08-01 02:23:54 +0200

    feat: 1.21.3-1.21.6 working

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -64,9 +64,16 @@ stonecutter {
 		match("1.21", "fabric", "neoforge")
 		match("1.21.1", "fabric", "neoforge")
 		match("1.21.2", "fabric", "neoforge")
-//		match("1.21.3", "fabric", "neoforge")
-//		match("1.21.4", "fabric", "neoforge")
+		match("1.21.3", "fabric", "neoforge")
+		match("1.21.4", "fabric", "neoforge")
+		match("1.21.5", "fabric", "neoforge")
+		match("1.21.6", "fabric", "neoforge")
+//		match("1.21.7", "fabric", "neoforge")
+//		match("1.21.8", "fabric", "neoforge")
+//		match("1.21.9", "fabric", "neoforge")
+//		match("1.21.10", "fabric", "neoforge")
+//		match("1.21.11", "fabric", "neoforge")
 
-		vcsVersion = "1.21.2-fabric"
+		vcsVersion = "1.21.6-fabric"
 	}
 }
