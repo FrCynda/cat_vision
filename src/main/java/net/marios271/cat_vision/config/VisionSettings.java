f37291d1853ca81c9b70b4c8553d243fad1270a5
FrCynda <sdellidj@yahoo.fr>
2026-08-21 21:18:47 +0200

    Fix curve backwards - it was brightest in daylight

--- src/main/java/net/marios271/cat_vision/config/VisionSettings.java
+++ src/main/java/net/marios271/cat_vision/config/VisionSettings.java
@@ -9,7 +9,11 @@ public class VisionSettings {
 	public double nv_speed = 0.01;
 
 	public double strengthFor(int light) {
-		double darkness = (light - nv_lit_light) / (double) nv_lit_light;
-		return nv_lit + (nv_dark - nv_lit) * Math.pow(darkness, nv_shape);
+		double darkness = (nv_lit_light - light) / (double) nv_lit_light;
+		return clamp(nv_lit + (nv_dark - nv_lit) * Math.pow(darkness, nv_shape), 0.0, 1.0);
+	}
+
+	public static double clamp(double value, double min, double max) {
+		return value < min ? min : Math.min(value, max);
 	}
 }
